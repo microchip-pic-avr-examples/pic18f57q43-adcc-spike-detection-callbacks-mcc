762d886034bf1c5269519bfc222edf17800807d8
M43959 <glen.nilsen@microchip.com>
2024-09-06 11:48:55 +0200

    MPAE-18221 initial MPLABX project creation

---
+++ pic18f57q43-adcc-spike-detection-callbacks-mcc.X/main.c
@@ -0,0 +1,87 @@
+ /*
+ * MAIN Generated Driver File
+ * 
+ * @file main.c
+ * 
+ * @defgroup main MAIN
+ * 
+ * @brief This is the generated driver implementation file for the MAIN driver.
+ *
+ * @version MAIN Driver Version 1.0.2
+ *
+ * @version Package Version: 3.1.2
+*/
+
+/*
+© [2024] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+#include "mcc_generated_files/system/system.h"
+
+static void ADCC_SpikeThreshold_Callback(void);
+static void ADCC_ConversionDone_Callback(void);
+static void Timer_Callback_100ms(void);
+
+static const struct TMR_INTERFACE *Timer = &Timer2; // TODO: Replace TimerX with name of const struct TMR_INTERFACE, from MCC Generated Files > timer > tmrx.c
+static volatile bool sendFrame = false;
+
+void ADCC_SpikeThreshold_Callback(void)
+{
+    DataStreamer.adcResult_lastSpike = ADCC_GetConversionResult();
+    DataStreamer.adcSpikeCount++;
+    IO_LED_Toggle();
+    IO_Debug_Toggle();
+    sendFrame = true;
+}
+
+void ADCC_ConversionDone_Callback(void)
+{
+    DataStreamer.adcResult = ADCC_GetConversionResult();  
+    sendFrame = true;
+}
+
+void Timer_Callback_100ms(void)
+{
+    ADCC_DischargeSampleCapacitor();
+    ADCC_StartConversion(channel_ANA7); // TODO: Replace x with the ADCC Channel you selected in Pin Grid View
+}
+
+int main(void)
+{
+    SYSTEM_Initialize();
+
+    DataStreamer.adcResult_lastSpike = 0;
+    DataStreamer.adcResult = 0;
+    DataStreamer.adcSpikeCount = 0;
+
+    ADCC_SetADTIInterruptHandler(ADCC_SpikeThreshold_Callback);
+    ADCC_SetADIInterruptHandler(ADCC_ConversionDone_Callback);
+    Timer->TimeoutCallbackRegister(Timer_Callback_100ms);
+
+    INTERRUPT_GlobalInterruptEnable();
+
+    while (1)
+    {
+        if (sendFrame)
+        {
+            WriteFrame();
+            sendFrame = false;
+        }
+    }
+}
